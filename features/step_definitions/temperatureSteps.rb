b0ad53c084befd1a5de542e38531d9bbcdb1646f
FranRosem <franklin.rosembluth@ucb.edu.bo>
2022-11-25 18:43:02 -0400

    Creating feature from Temperature Class

--- features/step_definitions/temperatureSteps.rb
+++ features/step_definitions/temperatureSteps.rb
@@ -3,11 +3,11 @@ Before do
 end
 
 Given /^I have entered (\d+) into the temperature changer$/ do |n|
-    @temperature.ftoc(n.to_i)
+    @temperature.push(n.to_i)
 end
 
-When /^I press (\w+)$/ do |celsius|
-  @result = @temperature.ftoc(celsius)
+When /^I press (\w+)$/ do |op|
+  @result = @temperature.op(op)
 end
 
 Then /^The result should be (\d+) on the screen$/ do |n|
